ffb68c5a5af12694a734d7b0a8995438005a2bef
hrpdevelopment <daniel@hetrandomportaal.nl>
2020-05-02 20:53:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on("message", async message => {
     }
 
     else if(command === `${prefix}alert`){
-        var i = 2 + messageArray.length;
+        const i = 2 + messageArray.length;
         
         var embed = new discord.MessageEmbed()
         .setTitle(messageArray[1])
