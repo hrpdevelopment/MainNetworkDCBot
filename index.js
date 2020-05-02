c9b9b0721d9663bff987999125b643f0dc154284
hrpdevelopment <daniel@hetrandomportaal.nl>
2020-05-02 22:04:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ client.on("message", async message => {
     }
 
     else if(command === `${prefix}alert`){
-        message.delete(300);
+        message.delete();
        
         msg = messageArray.slice(2).join(' ');
         var embed = new discord.MessageEmbed()
