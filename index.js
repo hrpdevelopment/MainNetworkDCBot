cb7425d77d690a547b221c974fcf6fd681ea00b1
hrpdevelopment <daniel@hetrandomportaal.nl>
2020-05-02 18:13:21 +0200

    init
    init

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const discord = require("discord.js");
 const botConfig = require("./botconfig.json");
 
 const client = new discord.Client();
-client.login(process.env.token);
 
 client.on("ready", async () => {
 
@@ -27,3 +26,4 @@ client.on("message", async message => {
     }
 
 })
+client.login(process.env.token);
